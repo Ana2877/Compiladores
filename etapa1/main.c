9b35bea42d7e481060eeb07ed66e7871b3eea253
Ana Carolina <acpagnoncelli@inf.ufrgs.br>
2021-02-06 17:43:02 -0300

    basic functions and file input in main working

--- etapa1/main.c
+++ etapa1/main.c
@@ -1,3 +1,43 @@
-int main(){
-	yylex();
+#include <stdlib.h>
+#include <stdio.h>
+
+//lex.yy.h
+int yylex();
+extern char *yytext;
+extern FILE *yyin;
+
+
+int isRunning(void);
+void initMe(void);
+int getLineNumber(void);
+
+int main(int argc, char **argv){
+
+	int token;
+
+	initMe();
+
+	if (argc < 2)
+    {
+    printf("call: ./etapa1 input.txt \n");
+    exit(1);
+    }
+  if (0==(yyin = fopen(argv[1],"r")))
+    {
+    printf("Cannot open file %s... \n",argv[1]);
+    exit(1);
+    }
+	while (isRunning()){
+		token = yylex();
+
+		if (!isRunning())
+			break;
+		printf(
+            "[%d] %d (%s)\n",
+            getLineNumber(),
+            token,
+            yytext);
+
+	}
+	printf("Acabou\n");
 }
--- etapa1/scanner.h
+++ etapa1/scanner.h
@@ -1,2 +1,8 @@
 #include "hash.h"
 #include "tokens.h"
+
+int running;
+int line_number;
+
+int isRunning(void);
+int getLineNumber(void);
